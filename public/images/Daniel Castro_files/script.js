927d61320cff12e245dc9582822c6984f15f6e8c
Daniel Castro <danielc13@ufl.edu>
2017-10-18 01:43:20 -0400

    Update landing image and colors Update About section

---
+++ public/images/Daniel Castro_files/script.js
@@ -0,0 +1,29 @@
+$("#arrow").click(function() {
+	$('html,body').animate({
+		scrollTop: $("#about").offset().top
+	}, 'slow');
+});
+
+$("#about-btn").click(function() {
+	$('html,body').animate({
+		scrollTop: $("#about").offset().top
+	}, 'slow');
+});
+
+$("#projects-btn").click(function() {
+	$('html,body').animate({
+		scrollTop: $("#projects").offset().top
+	}, 'slow');
+});
+
+$("#contact-btn").click(function() {
+	$('html,body').animate({
+		scrollTop: $("#contact").offset().top
+	}, 'slow');
+});
+
+$("#home-btn").click(function() {
+	$('html,body').animate({
+		scrollTop: $("#home").offset().top
+	}, 'slow');
+});
